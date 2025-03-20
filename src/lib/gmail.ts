6121ebcbdfac60d38c93bfc7c3b159d2dfaf0ff3
K5desu <dekinousyonn@gmail.com>
2025-03-20 18:47:53 +0900

    ルールの最適化及びコメントの最適化

--- src/lib/gmail.ts
+++ src/lib/gmail.ts
@@ -1,12 +1,28 @@
-export const checkSender = (): void => {
-  // Gmailの受信トレイからメールを取得
-  const threads = GmailApp.getInboxThreads(0, 10); // 受信トレイの最初の10スレッドを取得
+/**
+ * gmailの中の最新10件のメールを取得し、指定したドメインからのメールかどうかを確認する
+ * @param {string} domain メールの送信者のドメイン
+ */
+export const checkSender = (domain: string): void => {
+  /**
+   * Gmailの受信トレイから最新10件のメールを取得
+   */
+  const threads = GmailApp.getInboxThreads(0, 10);
+
+  // スレッドごとに処理
   threads.forEach((thread: GoogleAppsScript.Gmail.GmailThread) => {
-    const messages = thread.getMessages(); // スレッド内のすべてのメッセージを取得
+    /**
+     * スレッド内のメールを取得s
+     */
+    const messages = thread.getMessages();
+
+    // メールごとに処理
     messages.forEach((message: GoogleAppsScript.Gmail.GmailMessage) => {
-      const sender = message.getFrom(); // メールの送信者を取得
-      if (sender.includes('manaba@gmail.com')) {
-        // 送信者がmanaba@gmail.comを含む場合
+      // メールの送信者を取得
+      const sender = message.getFrom();
+
+      // メールの送信者が指定したドメインかどうかを確認
+      if (sender.includes(domain)) {
+        // ログにメールの送信者を出力
         console.log(`メールが見つかりました: ${sender}`);
       }
     });
